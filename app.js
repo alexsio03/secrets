860d2e5ba8302c5c9f1080b4a55476a540f63b72
Alex Warda <alexsio03@gmail.com>
2020-11-14 15:25:34 -0800

    Add local authentication

--- app.js
+++ app.js
@@ -4,24 +4,38 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res) {
   res.render('home');
@@ -35,20 +49,20 @@ app.route("/login")
 })
 
 .post(function(req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
+  const newUser = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
 
-  User.findOne({email: username}, function(err, foundUser) {
-    if (err) {
+  req.login(newUser, function(err) {
+    if(err) {
       console.log(err);
     } else {
-      if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        }
-      }
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/secrets");
+      });
     }
-  })
+  });
 });
 
 //-=-=-=-=-=- Register -=-=-=-=-=-=-
@@ -59,19 +73,33 @@ app.route("/register")
 })
 
 .post(function(req, res) {
-  const newUser = new User ({
-    email: req.body.username,
-    password: req.body.password
-  });
-
-  newUser.save(function(err) {
-    if (!err) {
-      res.render("secrets");
-    } else {
+  User.register({username: req.body.username}, req.body.password, function(err, user) {
+    if (err) {
       console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/secrets");
+      });
     }
-  });
+  })
+
 });
+
+//-=-=-=-=-=-=- Secrets -=-=-=-=-=-=-=-
+app.get("/secrets", function(req, res) {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+//-=-=-=-=-=-=- Logout -=-=-=-=-=-=-=-
+app.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
+})
 
 app.listen(3000, function() {
   console.log("Server running on port 3000");
